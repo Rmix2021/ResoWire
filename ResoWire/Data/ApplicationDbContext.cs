de5ce4adb8b6a705cf8c79460bd866cc894cb0ed
Rob Mix <eurotuner1981@gmail.com>
2022-02-08 23:02:33 +0100

    Initial Blazor with Identity build

---
+++ ResoWire/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResoWire.Data
+{
+    public class ResoWireDbContext : IdentityDbContext
+    {
+        public ResoWireDbContext(DbContextOptions<ResoWireDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
